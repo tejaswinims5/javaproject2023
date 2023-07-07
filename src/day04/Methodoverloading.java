1163cab83c7bf3ba7a04a322dd484d2d31bb3bc2
tejaswinims5 <138234207+tejaswinims5@users.noreply.github.com>
2023-07-07 20:20:01 +0530

    day04

--- src/day04/Methodoverloading.java
+++ src/day04/Methodoverloading.java
@@ -7,6 +7,9 @@ public class Methodoverloading {
        add(10,20);
        add(1230.3664f,7830.765f);
        add(1234.5674,125679.2436);
+       add(134,87,120);
+       Methodoverloading m=new Methodoverloading();
+       m.add(1234.873f, 23748.08633f, 136784.68733f);
 	}
 	
 	public static void add(int a,int b)
@@ -24,6 +27,16 @@ public class Methodoverloading {
 	
 	{
 		System.out.println(a+b);
+	}
+	
+	public static void add(int a, int b,int c)
+	{
+		System.out.println(a+b+c);
+	}
+	
+	public void add(float a,float b,float c)
+	{
+		System.out.println(a+b+c);
 	}
 }
 
