d4daec2c478c67d4770ed0e8c24f11b2534d532a
tejaswinims5 <138234207+tejaswinims5@users.noreply.github.com>
2023-07-07 20:05:49 +0530

    day04
    methodoverloading

---
+++ src/day04/Methodoverloading.java
@@ -0,0 +1,16 @@
+package day04;
+
+public class Methodoverloading {
+
+	public static void main(String[] args) {
+	
+       add(10,20);
+	}
+	
+	public static void add(int a,int b)
+	{
+		System.out.println(a+b);
+		
+	}
+
+}
