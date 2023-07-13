37d5c4905d0e2119bbad49f94334d8eb45a429ab
tejaswinims5 <138234207+tejaswinims5@users.noreply.github.com>
2023-07-13 20:11:31 +0530

    day7
    operator

---
+++ src/day07/Operators.java
@@ -0,0 +1,25 @@
+package day07;
+
+public class Operators {
+
+	public static void main(String[] args) {
+	        
+		int a;
+		int b;
+		double c;
+		double d;
+		a=20;
+		b=34;
+		c=234.093;
+		d=234.987;
+		
+		System.out.println(a+b);
+		System.out.println(a*c);
+		System.out.println(a/d);
+		System.out.println(c%d);
+		
+		
+
+	}
+
+}
