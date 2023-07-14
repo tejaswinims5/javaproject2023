b524b67894b0d084a27f5b29bfa33fa9764df07c
tejaswinims5 <138234207+tejaswinims5@users.noreply.github.com>
2023-07-14 20:16:43 +0530

    day08
    while and do while loop

---
+++ src/day08/Dowhileloop.java
@@ -0,0 +1,45 @@
+package day08;
+
+public class Dowhileloop {
+
+	public static void main(String[] args) {
+		
+
+int a=10;
+		
+		do
+		{
+		System.out.println("Good day..!");
+		a=a+1;
+		}while(a <=5);
+		
+		int b=5;
+		do
+		{
+			System.out.println("Java");
+			b=b-1;
+		}while(b>=1);
+		
+		
+		int c=-1;
+		do
+		{
+			System.out.println("Python");
+			c=c-1;
+		}while(c>=-5);
+		
+		
+		double d=0.1;
+		do
+		{
+			System.out.println("Selenium");
+			d=d+0.1;
+		}while(d<=0.5);
+		
+	}
+
+
+		
+	}
+
+
---
+++ src/day08/Whileloop.java
@@ -0,0 +1,44 @@
+package day08;
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		
+       int a=1;
+		
+		while(a <=5)
+		{
+		System.out.println("Good day..!");
+		a=a+1;
+		}
+		
+		
+		int b=5;
+		while(b>=1)
+		{
+			System.out.println("Java");
+			b=b-1;
+		}
+		
+		
+		int c=-1;
+		while(c>=-5)
+		{
+			System.out.println("Python");
+			c=c-1;
+		}
+		
+		
+		double d=0.1;
+		while(d<=0.5)
+		{
+			System.out.println("Selenium");
+			d=d+0.1;
+		}
+		
+	}
+
+
+	}
+
+
