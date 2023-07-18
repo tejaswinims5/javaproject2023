73cec71c55d45aca452d4b40f01cfeaeffa86966
tejaswinims5 <138234207+tejaswinims5@users.noreply.github.com>
2023-07-18 20:18:34 +0530

    day 09
    forloop

---
+++ src/day09/Forloop.java
@@ -0,0 +1,30 @@
+package day09;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		
+		for(int a=1;a<=5;a=a+1)
+		{
+			System.out.println("java");
+		}
+		
+		for(int a=5;a>=1;a=a-1)
+		{
+			System.out.println("selenium");
+		}
+		
+		for(int a=-5;a<=-1;a=a+1)
+			
+		{
+			System.out.println("selenium java");
+		}
+		
+		for(double a=0.1;a<=0.5;a=a+0.1)
+		{
+			System.out.println("java selenium");
+		}
+	}
+	
+
+}
