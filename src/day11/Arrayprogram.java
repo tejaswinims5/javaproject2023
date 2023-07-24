0bff1ac3f65f6d5fbd2c5297453eeba2f29e0f71
tejaswinims5 <138234207+tejaswinims5@users.noreply.github.com>
2023-07-24 20:38:59 +0530

    day11
    string array

--- src/day11/Arrayprogram.java
+++ src/day11/Arrayprogram.java
@@ -35,6 +35,8 @@ public class Arrayprogram {
 		{
 			System.out.println(x[s]);
 		}
+		y.close();
 	}
+	
 
 }
---
+++ src/day11/arraystringprog.java
@@ -0,0 +1,31 @@
+package day11;
+import java.util.Scanner;
+
+public class arraystringprog {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		Scanner x=new Scanner(System.in);
+		int size;
+		System.out.println("enter size of array");
+		size=x.nextInt();
+		String[] names=new String[size];
+		
+		System.out.println("enter string value for array");
+		for(int y=0;y<size;y++)
+		{
+			names[y]=x.next();
+			
+		}
+		for(int i=0;i<size;i++)
+		
+		{
+			System.out.println(names[i]);
+		}
+
+	}
+
+}
