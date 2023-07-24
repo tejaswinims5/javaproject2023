2a83775d81324df17ee7d88acdc56b7274ada0ed
tejaswinims5 <138234207+tejaswinims5@users.noreply.github.com>
2023-07-24 20:09:00 +0530

    day11
    array

---
+++ src/day11/Arrayprogram.java
@@ -0,0 +1,40 @@
+package day11;
+import java.util.Scanner;
+
+public class Arrayprogram {
+
+	public static void main(String[] args) {
+		
+		int x[];
+		x=new int[10];
+		
+		Scanner y=new Scanner(System.in);
+		
+		System.out.println("enter value for array");
+		for(int i=0;i<x.length;i++)
+		{
+			x[i]=y.nextInt();
+			
+		}
+		System.out.println("printing array values...........");
+
+		for(int i=0;i<x.length;i++)
+		{
+			System.out.println(x[i]);
+			
+		}
+		System.out.println("for each loop...........");
+		
+		for(int m:x)
+		{
+			System.out.println(m);
+		}
+		
+		System.out.println("reversing array");
+		for(int s=x.length-1;s>=0;s=s-1)
+		{
+			System.out.println(x[s]);
+		}
+	}
+
+}
